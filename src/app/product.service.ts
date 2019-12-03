591f0d5202ac37c3f1bf9d70d1ed8305524f148e
aaru <aaru123@gmail.com>
2019-12-03 19:46:06 +0530

    initial branch

---
+++ src/app/product.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  url = 'http://172.18.4.93:5000/product'
+
+  constructor(private http:HttpClient) { }
+
+  getProduct(){
+    return this.http.get(this.url)
+  }
+
+}
